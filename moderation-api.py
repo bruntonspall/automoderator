c1a9c948f39ec5342ee472cc3341920a5f4fe9ed
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2013-02-19 00:13:17 +0000

    Change to word pairs -> next word

--- moderation-api.py
+++ moderation-api.py
@@ -45,6 +45,10 @@ def moderate():
 def index():
     return render_template("index.html")
 
+@app.route("/generate")
+def generate():
+    return render_template("generate.html")
+
 
 if __name__ == "__main__":
     app.run()
--- wordcount.py
+++ wordcount.py
@@ -1,22 +1,31 @@
 import json
 import functools 
 from collections import defaultdict
-from itertools import repeat
+from itertools import repeat, izip
+import re
+import sys
 
 def add(i,j): 
     return i+j
 
-f = file('text1.txt')
+f = file(sys.argv[1])
 text = f.read()
 charsToReplace = ['\n', ':']
 replacedText = functools.reduce(lambda i,j: i.replace(j,''), charsToReplace, text)
-lines = [line.strip() for line in replacedText.split('.')]
-
+replacedText = re.sub("<[^>]*>", "", replacedText)
 d = defaultdict(lambda: defaultdict(int))
-combinations = functools.reduce(add, [zip(["*BEGIN*"]+line.split(' '), line.split(' ')+["*END*"]) for line in lines])
-for (k,v) in combinations: 
-    d[k][v] = d[k][v]+1
+lines = [line.strip()+'.' for line in replacedText.split('.') if line.strip() != '']
+beginnings = []
+for line in lines:
+    words = line.split(' ')
+    if len(words) > 2:
+        beginnings.append(words[0]+" "+words[1])
+        triples = izip(words, words[1:], words[2:])
+        for (one, two, nxt) in triples:
+            d[one+" "+two][nxt] += 1
 
 combos = dict([(k,reduce(add, [list(repeat(ik,iv)) for ik,iv in v.iteritems()])) for (k,v) in d.iteritems()])
-f = file('words.json', 'w')
-json.dump(combos, f)
+combos["*BEGIN*"] = beginnings
+print "Writing to file ",sys.argv[2]
+f = file(sys.argv[2], 'w')
+json.dump(combos, f, indent=2)
